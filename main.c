f1037e757f4b2cd45f82b5dd223f1c54be3fe93e
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 11:33:44 -0300

    Adiciona Rm do aluno

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main(){
     printf("Aluno: Thiago Soalheiro");
     printf("Primeiro projeto de checkpoint!");
     printf("Cursando meu primeiro ano em Ciências da Computação");
+    printf("Rm569316");
     return 0;
 }
